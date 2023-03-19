574cdcd4de1ae580e2843ce757f2a1497abe0076
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-20 00:46:58 +0300

    added restaurants serializer

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
     def index
         restaurants = Restaurant.all
-        render json: restaurants
+        render json: restaurants, status: :ok
     end
 end
