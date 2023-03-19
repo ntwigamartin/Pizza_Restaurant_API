a2464167632909b965ab427d8ee926f85a614aa3
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-20 01:17:49 +0300

    added restaurant show action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,20 @@
 class RestaurantsController < ApplicationController
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    
     def index
         restaurants = Restaurant.all
         render json: restaurants, status: :ok
+    end
+
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant, status: :ok
+    end
+
+    private
+
+    def not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
     end
 end
