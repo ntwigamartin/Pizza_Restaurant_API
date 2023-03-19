43777cb84c5b5e0e1f2cc44a6bb927d385e34899
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-19 20:31:07 +0300

    added restaurant resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :show, :destroy]
   resources :pizzas, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
